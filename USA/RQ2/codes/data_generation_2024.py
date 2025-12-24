613d90a5e58c3f7980bcbd411766f0add5d3d461
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-12-24 12:16:40 +0330

    Update data_generation_2024.py

--- USA/RQ2/codes/data_generation_2024.py
+++ USA/RQ2/codes/data_generation_2024.py
@@ -10,8 +10,8 @@ import random
 # Directory where THIS script lives
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATA_DIR = BASE_DIR / "data_test"
-OUTPUT_DIR = BASE_DIR / "data_test"
+DATA_DIR = BASE_DIR / "dataset_test"
+OUTPUT_DIR = BASE_DIR / "dataset_test"
 
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
