c0d450cc0eafb8c46ea766e39b5112828a106680
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2026-04-26 11:54:57 +0330

    Update open_source_fine_tune_vote_combined.py

--- Canada/RQ1/codes/open_source_fine_tune_vote_combined.py
+++ Canada/RQ1/codes/open_source_fine_tune_vote_combined.py
@@ -41,12 +41,12 @@ EVAL_BATCH_SIZE = 24
 MAX_TRAIN_LENGTH = 256
 MAX_PROMPT_LENGTH = 512
 
-DATA_PATH = "dataset_test/test_canada_election_vote_2021_3class.json"
+DATA_PATH = "dataset_test/test_canada_election_vote_2021_3class_replace.json"
 
 FINE_TUNE_FILES = [
     # "dataset_ft/agg_ft_vote.jsonl",
     # "dataset_ft/agg_ft_vote_2020_v2.jsonl",
-    "dataset_ft/individual_ft_vote.jsonl",
+    "dataset_ft/individual_ft_vote_replace.jsonl",
     # "dataset_ft/tweets_ft_vote_sample.jsonl",
 ]
 
