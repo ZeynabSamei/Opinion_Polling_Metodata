de77dddde6a2fa2730f9cd84d7a6aa9b1b48c293
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2026-04-27 13:24:43 +0330

    Update open_source_fine_tune_vote_combined.py

--- Canada/RQ1/codes/open_source_fine_tune_vote_combined.py
+++ Canada/RQ1/codes/open_source_fine_tune_vote_combined.py
@@ -31,10 +31,10 @@ FINE_TUNE_FILES = [
 ]
 
 MODELS = [
-    "meta-llama/Llama-3.1-8B-Instruct",
+    # "meta-llama/Llama-3.1-8B-Instruct",
     # "meta-llama/Llama-3.1-70B-Instruct",
-    "Qwen/Qwen2.5-7B-Instruct",
-    # "Qwen/Qwen2.5-14B-Instruct",
+    # "Qwen/Qwen2.5-7B-Instruct",
+    "Qwen/Qwen2.5-14B-Instruct",
 ]
 
 CANDIDATES = ["Justin Trudeau", "Erin O'Toole", "Others"]
