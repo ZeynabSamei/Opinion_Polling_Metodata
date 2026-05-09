7f5a4892a4624173935b5f6faa1c0efc14a8f203
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2026-05-09 13:46:57 +0330

    Update open_source_base_immigration.py

--- Canada/RQ2/codes/open_source_base_immigration.py
+++ Canada/RQ2/codes/open_source_base_immigration.py
@@ -31,7 +31,7 @@ DATA_PATH = "./dataset_test/test_canada_immigration_2024_new.json"
 MODELS = [
     "meta-llama/Llama-3.1-8B-Instruct",
     # "meta-llama/Llama-3.1-70B-Instruct",
-    # "Qwen/Qwen2.5-7B-Instruct",
+    "Qwen/Qwen2.5-7B-Instruct",
     # "Qwen/Qwen2.5-14B-Instruct",
 ]
 
