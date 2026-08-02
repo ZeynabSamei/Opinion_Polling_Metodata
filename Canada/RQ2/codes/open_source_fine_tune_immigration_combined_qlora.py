32d8f3b061374df3fef4fd0de14cfa8a938a4706
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2026-08-02 13:06:37 +0330

    Update open_source_fine_tune_immigration_combined_qlora.py

--- Canada/RQ2/codes/open_source_fine_tune_immigration_combined_qlora.py
+++ Canada/RQ2/codes/open_source_fine_tune_immigration_combined_qlora.py
@@ -46,10 +46,12 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--model_name",
         type=str,
-        # default="meta-llama/Llama-3.1-70B-Instruct",
+        default=[
+                # "meta-llama/Llama-3.1-70B-Instruct",
                 # "meta-llama/Llama-3.1-8B-Instruct",
                 # "Qwen/Qwen2.5-7B-Instruct",
-        default="Qwen/Qwen2.5-14B-Instruct" 
+                "Qwen/Qwen2.5-14B-Instruct"
+        ]
     )
     parser.add_argument(
         "--data_path",
